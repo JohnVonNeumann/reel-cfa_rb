fc56663c710e034493b18bcafec900efd12ba51d
JohnVonNeumann <louiswillcock@gmail.com>
2016-12-01 19:23:14 +1100

    added stripe api changes in order to get working and all that, working as intended, progressing onwards

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,10 +1,11 @@
 class ChargesController < ApplicationController
+
   def new
   end
 
   def create
-    # Amount in cents
-    @amount = 500
+    # Amount in cents. this needs to be changed when implemented on checkout
+    @amount =  500
 
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail],
@@ -15,7 +16,8 @@ class ChargesController < ApplicationController
       :customer    => customer.id,
       :amount      => @amount,
       :description => 'Rails Stripe customer',
-      :currency    => 'usd'
+      :currency    => 'usd',
+      :receipt_emaill => customer.email
     )
 
   rescue Stripe::CardError => e
