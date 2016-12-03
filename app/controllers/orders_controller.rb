5b73fe466ed3916e6fb1eabcbb54a906b18c6935
JohnVonNeumann <louiswillcock@gmail.com>
2016-12-03 12:48:52 +1100

    created a cart partial for rendering in other views upon checkout, altered orders_controller to include new before_actions and entered a new private method to be used by before_action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,8 @@
 class OrdersController < ApplicationController
+  include CurrentCart
   before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :set_cart, only: [:new, :create]
+  before_action :ensure_cart_isnt_empty, only: :new
 
   # GET /orders
   # GET /orders.json
@@ -70,5 +73,11 @@ class OrdersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
+    end
+
+    def ensure_cart_isnt_empty
+      if @cart.line_items.empty?
+        redirect_to store_index_url, notice: 'Your cart is empty'
+      end
     end
 end
